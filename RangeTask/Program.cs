ca5a8a33711ad4f49b01d8ba8516d5aee683413e
Артем Миронов <mr-smiles@yandex.ru>
2025-07-11 21:35:32 +0700

    Done

--- RangeTask/Program.cs
+++ RangeTask/Program.cs
@@ -4,7 +4,61 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!!!!");
+            // Создание объекта и задание его свойств
+
+            Range r1 = new Range();
+
+            Console.WriteLine(r1.From.ToString());
+            Console.WriteLine(r1.To.ToString());
+
+            r1.From = 2;
+            r1.To = 17.5;
+
+            Console.WriteLine(r1.From.ToString());
+            Console.WriteLine(r1.To.ToString());
+
+            // Проверка функции GetDistance
+
+            double distance = r1.GetDistance();
+            Console.WriteLine(distance.ToString());
+
+            // Проверка функции IsInside
+
+            double point1 = 0;
+            double point2 = 10;
+            Console.WriteLine(r1.IsInside(point1));
+            Console.WriteLine(r1.IsInside(point2));
+
+            // Проверка функции RangesIntersection
+
+            Range r2 = new Range(3.5, 20);
+            Range r3 = new Range(20, 25);
+            Range r4 = new Range(-15, -3);
+            Range r5 = new Range(-6, -2);
+
+            Range rangeIntersection1 = r1.RangesIntersection(r2);
+            Range rangeIntersection2 = r2.RangesIntersection(r3);
+            Range rangeIntersection3 = r4.RangesIntersection(r5);
+            Range rangeIntersection4 = r4.RangesIntersection(r3);           
+
+            // Проверка функции RangesUnion
+
+            Range[] rangeUnion1 = r1.RangesUnion(r2);
+            Range[] rangeUnion2 = r2.RangesUnion(r3);
+            Range[] rangeUnion3 = r3.RangesUnion(r4);
+            Range[] rangeUnion4 = r4.RangesUnion(r3);
+
+            // Проверка функции RangesDifference
+
+            Range r6 = new Range(1, 26);
+            Range r7 = new Range(-10, -7);
+
+            Range[] rangeDifference1 = r1.RangesDifference(r2);
+            Range[] rangeDifference2 = r2.RangesDifference(r3);
+            Range[] rangeDifference3 = r3.RangesDifference(r4);
+            Range[] rangeDifference4 = r4.RangesDifference(r5);
+            Range[] rangeDifference5 = r2.RangesDifference(r6);
+            Range[] rangeDifference6 = r4.RangesDifference(r7);
         }
     }
 }
--- RangeTask/Range.cs
+++ RangeTask/Range.cs
@@ -8,5 +8,95 @@ namespace RangeTask
 {
     internal class Range
     {
+        public double From { get; set; }
+        public double To { get; set; }
+
+        public Range(double from, double to)
+        {
+            this.From = from;
+            this.To = to;
+        }
+
+        public Range() : this(0, 0)
+        {
+        }
+
+        public double GetDistance()
+        {
+            return To - From;
+        }
+
+        public bool IsInside(double point)
+        {
+            return (From <= point) && (To >= point);
+        }
+
+        public Range RangesIntersection(Range range)
+        {
+            if ((From < range.From && To <= range.From) || (From > range.From && From >= range.To))
+            {
+                return null;
+            }
+
+            double maxFrom = Math.Max(this.From, range.From);
+            double minTo = Math.Min(this.To, range.To);
+
+            return new Range(maxFrom, minTo);
+        }
+
+        public Range[] RangesUnion(Range range)
+        {
+            if ((From < range.From && To < range.From) || (From > range.From && From > range.To))
+            {
+                Range[] rangeSeparateArray = new Range[2];
+                Range r1 = new Range(From, To);
+                Range r2 = new Range(range.From, range.To);
+                return new Range[2] { r1, r2 };
+            }
+
+            Range[] rangeArray = new Range[1];
+
+            double minFrom = Math.Min(this.From, range.From);
+            double maxTo = Math.Max(this.To, range.To);
+            rangeArray[0] = new Range(minFrom, maxTo);
+
+            return rangeArray;
+        }
+
+        public Range[] RangesDifference(Range range)
+        {
+            if (From >= range.From && To <= range.To)
+            {
+                Range r0 = new Range();
+                return new Range[1] { r0 };
+            }
+
+            if (From < range.From && To > range.To)
+            {                
+                Range r1 = new Range(From, range.From);
+                Range r2 = new Range(range.To, To);
+                return new Range[2] { r1, r2 };
+            }
+
+            if ((From <= range.From && To <= range.From) || (From >= range.From && From >= range.To))
+            {                
+                Range  r1 = new Range(From, To);                
+                return new Range[1] { r1 };
+            }
+
+            if (From < range.To)
+            {
+                Range r2 = new Range(From, range.From);
+                return new Range[1] { r2 };
+            }
+
+            if (To > range.From)
+            {
+                Range r3 = new Range(range.From, From);
+                return new Range[1] { r3 };
+            }
+
+            return null;
+        }
     }
 }
